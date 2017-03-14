ad2143b256d7c2b30922ebd951f33544e875714f
ScepticMatt <ScepticMatt@users.noreply.github.com>
2017-03-14 16:44:20 +0100

    Update clock.cpp

--- clock.cpp
+++ clock.cpp
@@ -7,7 +7,6 @@ using namespace std::chrono;
 // testing time difference between prefix and postfix iteration
 int main ()
 {
-  
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
   for (int i = 0; i < INT_MAX; i++);
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
@@ -21,6 +20,5 @@ int main ()
   std::cout << "time2: " << time_span2.count() << " seconds." << std::endl;
   std::cout << "time diff" << 100*percentage << " \%" << std::endl;
   std::cout << std::endl;
-
   return 0;
 }
